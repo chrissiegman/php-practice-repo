5f1fd0439a8e53f6ac50a6ffff3020d924cbda80
chris <chrissiegman@yahoo.com>
2015-12-01 11:35:16 -0600

    Add construct/destruct practice

---
+++ construct_destruct.php
@@ -0,0 +1,35 @@
+<?php
+
+class User {
+
+    public $username;
+
+    function __construct($username, $password) {
+        // constructor code goes here
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    function print_info() {
+        print "Username: $this->username<br>";
+        print "Password: $this->password<br>";
+    }
+
+    function change_info($username, $password) {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    function __destruct(){
+        print "Destroying $this->username";
+    }
+}
+
+$bob = new User('Bob', '123');
+
+$bob->print_info();
+$bob->change_info('Robert', 'password');
+$bob->print_info();
+
+
+?>
