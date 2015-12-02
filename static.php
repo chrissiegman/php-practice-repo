e5b0e5e478abab16c0816a44938987f04220e95a
chris <chrissiegman@yahoo.com>
2015-12-02 09:14:37 -0600

    Extend static example

--- static.php
+++ static.php
@@ -3,13 +3,26 @@
 User::pwd_string();
 
 class User {
-
     static function pwd_string() {
-        echo "Please enter your password: ";
+        echo "Please enter your password:<br>";
     }
-
 }
 
+$temp = new Test();
+
+
+echo "Test A: " . Test::$static_property . "<br>";
+echo "Test B: " . $temp->get_sp()        . "<br>";
+echo "Test C: " . $temp->static_property . "<br>"; // fails because $static_property is not accessible to $temp
+
+class Test {
+    static $static_property = "I'm static";
+
+    function get_sp() {
+        return self::$static_property;            // self keyword is how to access a static property or constant
+                                                  // directly from within a class
+    }
+}
 
 ?>
 
