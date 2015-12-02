6971bed15a46fc1577b9d03770ec2f44d79958e2
chris <chrissiegman@yahoo.com>
2015-12-02 17:42:21 -0600

    Clean up scraper script

--- snt-media-project/scraper.php
+++ snt-media-project/scraper.php
@@ -4,10 +4,8 @@ $url = "http://www.cnn.com/2015/12/02/europe/nato-montenegro-membership-invitati
 $ch = curl_init($url);
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$curl_scraped_page = curl_exec($ch);
+$html = curl_exec($ch);
 curl_close($ch);
-
-$html = $curl_scraped_page;
 
 $dom = new DOMDocument;
 $dom->loadHTML($html);
@@ -19,17 +17,12 @@ $p_array = array();
 
 foreach ($headline as $headline) {
     array_push($h_array, $headline->nodeValue);
-    //echo $headline->nodeValue;
-    //echo "<br>";
 }
 
 $text_paragraphs = $dom->getElementsByTagName('p');
 
 foreach ($text_paragraphs as $p) {
     array_push($p_array, $p->nodeValue);
-    //echo "<br>";
-    //echo $p->nodeValue;
-    //echo "<br>";
 }
 
 $content_dict = array("headline"=>$h_array, "paragraphs"=>$p_array);
