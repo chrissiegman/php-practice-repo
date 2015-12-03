274005f476fb8de9936ba56590083d69a02ba9d1
chris <chrissiegman@yahoo.com>
2015-12-03 11:30:36 -0600

    Finish scraper

--- snt-media-project/scraper.php
+++ snt-media-project/scraper.php
@@ -1,19 +1,29 @@
 <?php
-$scraper = new Scraper('www.cnn.com/2015/12/02/europe/nato-montenegro-membership-invitation/index.html');
+
+// Example usage of scraper:
+
+$url = 'www.cnn.com/2015/12/02/europe/nato-montenegro-membership-invitation/index.html';
+$scraper = new Scraper($url);
 print_r($scraper->produce_json());
+
+
 class Scraper {
+
     function __construct($url) {
-        // Retrieve html from given url (ADD ERROR HANDLING)
+
+        // Retrieve html from given url
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $this->html = curl_exec($ch);
         curl_close($ch);
-        // Create DOMDocument object (ADD ERROR HANDLING)
+        
+        // Create DOMDocument object
         $this->dom = new DOMDocument;
         $this->dom->loadHTML($this->html);
     }
+
     // Arrange text content into an array
-    function gather_content_by_tag($tag) {
+    function gather_text_by_tag($tag) {
         $content_array = array();
         $nodes = $this->dom->getElementsByTagName($tag);
         foreach ($nodes as $node) {
@@ -21,22 +31,37 @@ class Scraper {
         }
         return $content_array;
     }
-    function produce_content_dict () {
-        $h_array = Scraper::gather_content_by_tag('h1');
-        $p_array = Scraper::gather_content_by_tag('p');
-        return array('headline'=>$h_array, 'paragraphs'=>$p_array);
+
+    function gather_images() {
+        $images_array = array();
+        $images = $this->dom->getElementsByTagName('img');
+        foreach ($images as $image) {
+            $image_source = $image->getAttribute('src');
+
+            // filters out unuseable img tags
+            $pos = strpos($image_source, 'jpg');
+            if ($pos == true) {
+                array_push($images_array, $image_source);
+            }
+            
+        }
+        return $images_array;
     }
+
+    // Creates associative array suitable for JSON encoding
+    function format_content() {
+        $h_array = self::gather_text_by_tag('h1');
+        $p_array = self::gather_text_by_tag('p');
+        $i_array = self::gather_images();
+        return array('headline'=>$h_array, 'paragraphs'=>$p_array, 'images'=>$i_array);
+    }
+
     function produce_json() {
-        return json_encode(Scraper::produce_content_dict());
+        return json_encode(self::format_content());
     }
-    // examine image object to see properties and fill this in.
-    function gather_image_by_tag () {
-    }
-    // Clone page (for debugging)
-    function clone_page() {
-        echo $this->html;
-    }
+
     function __destruct() {
     }
+
 }
 ?>
